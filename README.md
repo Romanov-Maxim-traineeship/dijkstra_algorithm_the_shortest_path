9dc62dbdfa15510b29066f292036b100423bd883
Maxim Romanov <maxim.romanov@jetrockets.ru>
2019-03-06 22:55:41 +0300

    rebase change
    some fix
    
    fix'
    
    fix'
    
    govno code
    
    some fix
    
    minify
    
    Create README.md
    
    Update README.md
    
    Update README.md
    
    minify the code
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md
    
    Update README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+###  Работа из терминала
+
+Для начала нужно установить пакет "readline-sync" через npm или yarn 
+```bash
+npm install readline-sync -S
+или
+yarn add readline-sync
+```
+
+### Чтобы начать введите в папке с файлом 
+```bash
+node index.js 
+```
+
+### `∞ - Infinity`
--- index.js
+++ index.js
@@ -1,83 +1,62 @@
-function makeArray(height, width) {
+const readline = require('readline-sync');
+var way = [];
+
+const makeArray = (height, width) => {
   let outputArray = new Array(height);
   let row = new Array(width);
   for (let iy = 0; iy < height; iy++) {
     outputArray[iy] = row.slice();
   }
   return outputArray;
-}
-const infinity = 999;
-array = makeArray(6, 6);
-result_array = makeArray(6, 4);
+};
 
-array[0][0] = 0;
-array[0][1] = 1;
-array[0][2] = infinity;
-array[0][3] = 10;
-array[0][4] = 9;
-array[0][5] = infinity;
-
-array[1][0] = 1;
-array[1][1] = 0;
-array[1][2] = 8;
-array[1][3] = 2;
-array[1][4] = 11;
-array[1][5] = infinity;
-
-array[2][0] = infinity;
-array[2][1] = 8;
-array[2][2] = 0;
-array[2][3] = 3;
-array[2][4] = 6;
-array[2][5] = 4;
-
-array[3][0] = 10;
-array[3][1] = 2;
-array[3][2] = 3;
-array[3][3] = 0;
-array[3][4] = infinity;
-array[3][5] = 5;
-
-array[4][0] = 9;
-array[4][1] = 11;
-array[4][2] = 6;
-array[4][3] = infinity;
-array[4][4] = 0;
-array[4][5] = 7;
-
-array[5][0] = infinity;
-array[5][1] = infinity;
-array[5][2] = 4;
-array[5][3] = 5;
-array[5][4] = 7;
-array[5][5] = 0;
-
-for (let j = 0; j < array.length; j++) {
-  result_array[j][0] = array[j][array.length - 1];
-}
-
-var number = [];
-
-for (let j = 0; j < array.length; j++) {
-  var min = infinity;
+const fillArray = (array) => {
+  console.log('Введи путь: ');
   for (let i = 0; i < array.length; i++) {
-    if (array[i][j] + result_array[i][0] < min) {
-      min = array[i][j] + result_array[i][0];
-    }
-  }
-  result_array[j][1] = min;
-}
-
-for (let k = 0; k < result_array.length; k++) {
-  for (let j = 0; j < array.length; j++) {
-    var min = infinity;
-    for (let i = 0; i < array.length; i++) {
-      if (array[i][j] + result_array[i][k] < min) {
-        min = array[i][j] + result_array[i][k];
+    for (let j = 0; j < array[i].length; j++) {
+      if (i == j) {
+        array[i][j] = 0;
+        continue;
+      } else if (typeof array[j][i] !== 'undefined') {
+        array[i][j] = array[j][i];
+        continue;
       }
-    }
-    result_array[j][k + 1] = min;
-  }
-}
+      const numberForArray = readline.question(`${i + 1} -> ${j + 1} `);
 
-console.log(result_array);
+      array[i][j] = +numberForArray;
+    }
+  }
+  return array;
+};
+
+const shortestPathTable = (array = [], way = []) => {
+  let resultArray = makeArray(array.length, array.length - 1);
+  for (let k = 0; k < array.length - 2; k++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      let min = (currentPoint = Infinity);
+      for (let i = 0; i < array[j].length; i++) {
+        resultArray[i][0] = array[i][array.length - 1];
+        resultArray[resultArray.length - 1][j] = 0;
+        if (i == j) continue;
+        var sum = array[i][j] + resultArray[i][k];
+        if (sum < min) {
+          min = sum;
+          currentPoint = i;
+        }
+      }
+      way.push(`${j + 1} -> ${currentPoint + 1}`);
+      resultArray[j][k + 1] = min;
+    }
+  }
+  return resultArray;
+};
+
+console.clear();
+const integerSize = +readline.question('Количество точек? ');
+console.clear();
+answer = fillArray(makeArray(integerSize, integerSize));
+
+console.clear();
+console.table(answer);
+console.table(shortestPathTable(answer, way));
+console.table(way.slice(-integerSize + 1));

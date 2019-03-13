40ebc2aee75a95110ddca465607e3032c4f1ee7c
Maxim Romanov <maxim.romanov@jetrockets.ru>
2019-03-14 01:21:49 +0300

    minify the code

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
-const readline = require('readline-sync'),
-  makeArray = (r, e) => {
-    let a = Array(r),
-      n = Array(e);
-    for (let e = 0; r > e; e++) a[e] = n.slice();
+const readline = require('readline-sync');
+var way = [];
+const makeArray = (a, b) => {
+    let c = Array(a),
+      d = Array(b);
+    for (let e = 0; e < a; e++) c[e] = d.slice();
+    return c;
+  },
+  fillArray = (a) => {
+    console.log('\u0412\u0432\u0435\u0434\u0438 \u043F\u0443\u0442\u044C: ');
+    for (let b = 0; b < a.length; b++)
+      for (let c = 0; c < a[b].length; c++) {
+        if (b == c) {
+          a[b][c] = 0;
+          continue;
+        } else if ('undefined' != typeof a[c][b]) {
+          a[b][c] = a[c][b];
+          continue;
+        }
+        const d = readline.question(`${b + 1} -> ${c + 1} `);
+        a[b][c] = +d;
+      }
     return a;
   },
-  fillArray = (r) => {
-    console.log('Введи путь: ');
-    for (let e = 0; e < r.length; e++)
-      for (let a = 0; a < r[e].length; a++) {
-        if (e == a) {
-          r[e][a] = 0;
-          continue;
-        }
-        if (void 0 !== r[a][e]) {
-          (r[e][a] = r[a][e]), console.log(r[a][e]);
-          continue;
-        }
-        const n = readline.question(`${e + 1} -> ${a + 1} `);
-        r[e][a] = +n;
+  shortestPathTable = (a = [], b = []) => {
+    let c = makeArray(a.length, a.length - 1);
+    for (let e = 0; e < a.length - 2; e++)
+      for (let f, g = 0; g < a.length - 1; g++) {
+        f = currentPoint = 1 / 0;
+        for (let b = 0; b < a[g].length; b++)
+          if (
+            ((c[b][0] = a[b][a.length - 1]), (c[c.length - 1][g] = 0), b != g)
+          ) {
+            var d = a[b][g] + c[b][e];
+            d < f && ((f = d), (currentPoint = b));
+          }
+        b.push(`${g + 1} -> ${currentPoint + 1}`), (c[g][e + 1] = f);
       }
-  },
-  shortestPathTable = (r, e) => {
-    for (let t = 0; t < e[t].length; t++)
-      for (let i = 0; i < r.length; i++) {
-        var a = 999;
-        for (let l = 0; l < r[i].length; l++) {
-          e[l][0] = r[l][r.length - 1];
-          var n = r[l][i] + e[l][t - 1];
-          a > n && (a = n);
-        }
-        e[i][t] = a;
-      }
-  },
-  integerSize = +readline.question('Количество точек? ');
-(array = makeArray(integerSize, integerSize)),
-  (result_array = makeArray(integerSize, integerSize - 1)),
-  fillArray(array),
-  shortestPathTable(array, result_array),
-  console.table(array),
-  console.table(result_array);
+    return c;
+  };
+console.clear();
+const integerSize = +readline.question(
+  '\u041A\u043E\u043B\u0438\u0447\u0435\u0441\u0442\u0432\u043E \u0442\u043E\u0447\u0435\u043A? '
+);
+console.clear(),
+  (answer = fillArray(makeArray(integerSize, integerSize))),
+  console.clear(),
+  console.table(answer),
+  console.table(shortestPathTable(answer, way)),
+  console.table(way.slice(-integerSize + 1));
